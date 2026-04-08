7f07e906cfa90670b66ca9365c9b63fc3a566264
haodo04 <22130077@st.hcmuaf.edu.vn>
2026-04-08 11:49:41 +0700

    feat: base app component and css

---
+++ frontend/src/assets/data.js
@@ -0,0 +1,8 @@
+export const features = [
+    {
+        iconName: "ArrowUpCircle",
+        iconColor: "text-purple-500",
+        title: "Easy File Upload",
+        description: "Quickly upload your files with our intuitive drag-and drop interface."
+    },
+]
